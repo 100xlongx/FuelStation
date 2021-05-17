40955c8fbfd46b6043a9f68fc61076da1a8b2947
Raaed Khan <raaedk@gmail.com>
2021-05-17 15:43:34 -0400

    implemented car strategy

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FuelStation
+{
+    class FuelStation {
+        public decimal fuelCar(int currentFuel, IFuelStrategy strategy) {
+            return strategy.fuelCar(currentFuel);
+        }
+    }
+
+    public interface IFuelStrategy {
+        decimal getPrice();
+        decimal fuelCar(int currentFuel);
+    }
+
+    public class Car : IFuelStrategy {
+        public int maxFuel { get {return 12;} set {maxFuel = value;} }
+
+        public decimal fuelCar(int currentFuel) {
+            return (maxFuel - currentFuel) * this.getPrice();
+        }
+
+        public decimal getPrice() {
+            return 3.00M;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fuelStation = new FuelStation();
+            var carStrat = new Car();
+
+            Console.WriteLine(fuelStation.fuelCar(9, carStrat));
+        }
+    }
+}
